c4c80f40f6d2ddd3264a805b91829ab4e1d5c53f
zeroclock <zeroclock@icloud.com>
2020-06-28 15:18:53 +0900

    Add datastore integration

---
+++ backend/config/config.go
@@ -0,0 +1,17 @@
+package config
+
+type Config struct {
+	DSConfig *DatastoreConfig
+}
+
+type DatastoreConfig struct {
+	ProjectID string
+}
+
+func GetConfig() *Config {
+	return &Config{
+		DSConfig: &DatastoreConfig{
+			ProjectID: "taskiwi-dev",
+		},
+	}
+}
--- backend/handler/router.go
+++ backend/handler/router.go
@@ -1,16 +1,37 @@
 package handler
 
 import (
-	"github.com/labstack/echo"
-	"net/http"
+	"context"
 	"log"
+	"net/http"
+
+	"cloud.google.com/go/datastore"
+	"github.com/labstack/echo"
+	"taskiwi/config"
 )
 
 func InitRouting(e *echo.Echo) {
 	e.GET("/", helloHandler)
 }
 
+type Employee struct {
+	Name string
+}
+
 func helloHandler(c echo.Context) error {
 	log.Println("hello action")
+
+	ctx := context.Background()
+	config := config.GetConfig()
+	datastoreClient, _ := datastore.NewClient(ctx, config.DSConfig.ProjectID)
+	e := &Employee{
+		Name: "tarou tanaka",
+	}
+	k := datastore.IncompleteKey("Employee", nil)
+	_, err := datastoreClient.Put(ctx, k, e)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return c.String(http.StatusOK, "Hello")
 }
