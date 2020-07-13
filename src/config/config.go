7f3858f7330658c44b2f4b174bffab29fe861656
zeroclock <zeroclock@icloud.com>
2020-07-13 09:40:15 +0900

    Add method to parse csv and store global json data

--- src/config/config.go
+++ src/config/config.go
@@ -1,17 +1,73 @@
 package config
 
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"strings"
+)
+
 type Config struct {
-	DSConfig *DatastoreConfig
+	IData *InputData
+	CData *[]ClockData
 }
 
-type DatastoreConfig struct {
-	// ProjectID string
+type InputData struct {
+	FilePath    string
+	FileContent [][]string
 }
 
-func GetConfig() *Config {
+type ClockData struct {
+	Task     string   `json:"task"`
+	Parents  string   `json:"parents"`
+	Category string   `json:"category"`
+	Start    string   `json:"start"`
+	End      string   `json:"end"`
+	Effort   string   `json:"effort"`
+	Ishabit  string   `json:"ishabit"`
+	Tags     []string `json:"tags"`
+}
+
+var GlobalConf *Config
+
+func InitConfig(path string) *Config {
+	csvFile, err := os.Open(path)
+	if err != nil {
+		log.Println(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		log.Println(err)
+	}
+
+	idata := InputData{
+		FilePath:    path,
+		FileContent: csvData,
+	}
+
+	var cdata ClockData
+	var cdatas []ClockData
+
+	for _, each := range csvData {
+		cdata.Task = each[0]
+		cdata.Parents = each[1]
+		cdata.Category = each[2]
+		cdata.Start = each[3]
+		cdata.End = each[4]
+		cdata.Effort = each[5]
+		cdata.Ishabit = each[6]
+		cdata.Tags = strings.Split(each[7], ":")
+
+		cdatas = append(cdatas, cdata)
+	}
+
 	return &Config{
-		DSConfig: &DatastoreConfig{
-			// ProjectID: "taskiwi-dev",
-		},
+		IData: &idata,
+		CData: &cdatas,
 	}
 }
--- src/main.go
+++ src/main.go
@@ -2,36 +2,40 @@ package main
 
 import (
 	"taskiwi/handler"
+	"taskiwi/config"
 
+	"flag"
+	"fmt"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"flag"
-	"log"
-	"fmt"
-	"os"
 	"io/ioutil"
+	"log"
+	"os"
 )
 
 func main() {
-    var pidFile = flag.String("pid-file", "", "./")
-    flag.Parse()
+	var pidFile = flag.String("pid-file", "", "./")
+	flag.Parse()
 
-    if len(*pidFile) > 0 {
-        if err := ioutil.WriteFile(*pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664); err != nil {
-            log.Printf("[WARNING] Failed to write pid file. %v\n", err)
-        }
-        defer func() {
-            if err := os.Remove(*pidFile); err != nil {
-                log.Printf("[WARNING] Failed to delete pid file. %v\n", err)
-            }
-        }()
-    }
+	if len(*pidFile) > 0 {
+		if err := ioutil.WriteFile(*pidFile, []byte(fmt.Sprintf("%d", os.Getpid())), 0664); err != nil {
+			log.Printf("[WARNING] Failed to write pid file. %v\n", err)
+		}
+		defer func() {
+			if err := os.Remove(*pidFile); err != nil {
+				log.Printf("[WARNING] Failed to delete pid file. %v\n", err)
+			}
+		}()
+	}
+
+	path := "./test.csv"
+	config.GlobalConf = config.InitConfig(path)
+	log.Println(config.GlobalConf.IData.FileContent)
 
 	e := echo.New()
 	e.Static("/", "./web/taskiwi/build")
 	handler.InitRouting(e)
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
-	e.Use(handler.FirebaseAuthMiddleware)
 	e.Logger.Fatal(e.Start(":8080"))
 }
